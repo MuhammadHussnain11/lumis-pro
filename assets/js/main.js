300b1969e19a1f6ce1c0560633fb588a80b540e4
Muhammad Hussnain <maztarza@gmail.com>
2025-02-06 17:44:41 +0500

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,17 @@ function closeMenu(){
     menu_.classList.remove('active');
     body.classList.remove('overflow');
 }
+
+
+const sections = document.querySelectorAll('.ph-sec__ .row');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            sections.forEach(sec => sec.classList.remove('active'));
+            entry.target.classList.add('active');
+        }
+    });
+}, { threshold: 0.5 });
+
+sections.forEach(section => observer.observe(section));
